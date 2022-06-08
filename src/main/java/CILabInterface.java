020225d9ae24b3a59ecf22f7eae8ba34fa96b6fc
Alex Levasseur <chatterboxlevasseur@gmail.com>
2022-06-08 11:37:08 -0700

    travis test

--- src/main/java/CILabInterface.java
+++ src/main/java/CILabInterface.java
@@ -15,7 +15,7 @@ public interface CILabInterface {
     void setString(String string);
 
     /**
-     *  We define the usage of capitals in a word to be right when one of the following cases holds:
+     *   define the usage of capitals in a word to be right when one of the following cases holds:
      *  All letters in this word are capitals, like "USA".
      *  All letters in this word are not capitals, like "leetcode".
      *  Only the first letter in this word is capital, like "Google".
