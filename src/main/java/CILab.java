7cc7f034630c940f3e3f4341639f10d803730c01
Alex Levasseur <chatterboxlevasseur@gmail.com>
2022-06-08 12:16:08 -0700

    Added functionionality

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -13,7 +13,33 @@ public class CILab implements CILabInterface {
 
     @Override
     public boolean detectCapitalUse() {
-        
+    	boolean result = false;
+    	boolean isPreviousUpper = false;
+        for (int i = 0; i < myString.length(); i++) {
+        	if(i == 0 && myString[i].isUpperCase()) {
+        		isPreviousUpper = true;
+        	}
+        	else if (i = 1 && !myString[i].isUpperCase()) {
+        		isPreviousUpper = false;
+        	}
+        	else if(myString[i].isUpperCase()) {
+        		if(isPreviousUpper) {
+        			result = true;
+        		}
+        		else {
+        			result = false;
+        		}
+        	}
+        	else {
+        		if(!isPreviousUpper) {
+        			result = true;
+        		}
+        		else {
+        			result = false;
+        		}
+        	}
+        }
+        return result;
     }
 
 }
